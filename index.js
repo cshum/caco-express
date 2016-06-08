ea4c13c1bd624b5fc09f9f102dec196989ff7ccc
Adrian Shum <adrianshum@ecargo.com>
2016-06-08 15:26:00 +0800

    use standard, aad jsdoc

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var caco = require('caco')
 
+/**
+ * caco generator function wrapper for express middleware
+ *
+ * @param {function*} genFn - generator function
+ * @returns {function} express middleware function
+ */
 module.exports = function (genFn) {
   var asyncFn = caco.wrap(genFn)
+  /* 
+   * - catch async error, callback to express next(err)
+   * - args.length 0 for explicitly calling next(), 
+   *   which should pass to next express middleware
+   */
   if (genFn.length === 4) {
     // 4 args, return express error handling middleware
     return function middleware (err, req, res, next) {
@@ -9,7 +20,7 @@ module.exports = function (genFn) {
         if (err) return next(err)
         if (arguments.length === 0) return next()
       })
-    };
+    }
   } else {
     // return normal express middleware
     return function middleware (req, res, next) {

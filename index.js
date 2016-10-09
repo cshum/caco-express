17fca668163e63b48036aa4d71ea34bb73bad461
Adrian Shum <me@cshum.com>
2016-10-09 18:44:48 +0800

    add factory method

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var raco = require('raco')
+function isFn (fn) {
+  return typeof fn === 'function'
+}
+function isRaco (obj) {
+  return isFn(obj) && isFn(obj.wrap) && isFn(obj.wrapAll)
+}
 
 /**
  * Express middleware wrapper using raco generator function
@@ -6,36 +12,31 @@ var raco = require('raco')
  * @param {...function*} genFn - generator function
  * @returns {function} express middleware function
  */
-module.exports = function wrap (genFn) {
-  // return array if over 1 args of genFn
-  var args = Array.prototype.slice.call(arguments)
-  if (args.length > 1) {
-    return args.map(function (genFn) {
-      return wrap(genFn)
-    })
-  }
-
-  /*
-   * - catch async error, callback to express next(err)
-   * - args.length 0 for explicitly calling next(),
-   *   which should pass to next express middleware
-   */
-  var asyncFn = raco.wrap(genFn)
-  if (genFn.length === 4) {
-    // 4 args, return express error handling middleware
-    return function (err, req, res, next) {
-      return asyncFn.call(this, err, req, res, function (err) {
-        if (err) return next(err)
-        if (arguments.length === 0) return next()
-      })
-    }
-  } else {
-    // return normal express middleware
-    return function (req, res, next) {
-      return asyncFn.call(this, req, res, function (err) {
-        if (err) return next(err)
-        if (arguments.length === 0) return next()
-      })
+module.exports = (function factory (raco) {
+  return function (genFn, opts) {
+    if (isRaco(genFn)) return factory(genFn)
+    /*
+     * - catch async error, callback to express next(err)
+     * - args.length 0 for explicitly calling next(),
+     *   which should pass to next express middleware
+     */
+    var asyncFn = raco.wrap(genFn, opts)
+    if (genFn.length === 4) {
+      // 4 args, return express error handling middleware
+      return function (err, req, res, next) {
+        return asyncFn.call(this, err, req, res, function (err) {
+          if (err) return next(err)
+          if (arguments.length === 0) return next()
+        })
+      }
+    } else {
+      // return normal express middleware
+      return function (req, res, next) {
+        return asyncFn.call(this, req, res, function (err) {
+          if (err) return next(err)
+          if (arguments.length === 0) return next()
+        })
+      }
     }
   }
-}
+})(raco)
--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 var test = require('tape')
 var express = require('express')
 var request = require('supertest')
+var raco = require('raco')
 var wrap = require('./')
+var wrapPrepend = require('./')(raco({ prepend: true }))
 
 var app = express()
 app.get('/foo', wrap(function * (req, res, next) {
@@ -11,21 +13,21 @@ app.get('/foo', wrap(function * (req, res, next) {
   }, 5)
   yield setTimeout(next, 10)
   next()
-}, function * (req, res) {
+}), function (req, res) {
   res.json({ foo: res.arr })
-}))
+})
 
 app.get('/boom', wrap(function * (req, res, next) {
   yield setTimeout(next, 0)
   throw new Error('boom')
 }))
 
-app.get('/689', wrap(function * (req, res, next) {
+app.get('/689', wrap(function * (next, req, res) {
   next(new Error('169'))
   yield setTimeout(next, 0)
-}))
+}, { prepend: true }))
 
-app.use(wrap(function * (err, req, res, next) {
+app.use(wrapPrepend(function * (next, err, req, res) {
   res.status(500)
   res.json({ success: false, message: err.message })
 }))

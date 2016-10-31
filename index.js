e85d6835e5716651d68a2423cd5c510c33c32985
Adrian Shum <adrianshum@ecargo.com>
2016-10-31 09:48:54 +0800

    clean up

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ var methods = require('methods')
 var flatten = require('array-flatten')
 
 function isFunction (val) {
-  return typeof val === 'function'
+  return val && typeof val === 'function'
 }
+
 function isGenerator (val) {
   return val && isFunction(val.next) && isFunction(val.throw)
 }
+
 function isGeneratorFunction (val) {
   if (!val || !val.constructor) return false
   if (val.constructor.name === 'GeneratorFunction' || val.constructor.displayName === 'GeneratorFunction') return true
   return isGenerator(val.constructor.prototype)
 }
+
 function isExpress (val) {
   return val && isFunction(val.all) && isFunction(val.post) && isFunction(val.get)
 }
+
 function wrapMethod (fn, opts) {
   return function () {
     var args = flatten(Array.prototype.slice.call(arguments)).map(function (fn) {
@@ -28,11 +32,6 @@ function wrapMethod (fn, opts) {
 function wrap (app, opts) {
   if (isGeneratorFunction(app)) {
     // app is middleware generator function
-    /*
-     * - catch async error, callback to express next(err)
-     * - args.length 0 for explicitly calling next(),
-     *   which should pass to next express middleware
-     */
     var asyncFn = raco.wrap(app, opts)
     if (app.length === 4) {
       // 4 args, return express error handling middleware
@@ -40,6 +39,7 @@ function wrap (app, opts) {
         return asyncFn.call(this, err, req, res, function (err) {
           if (err) return next(err)
           if (arguments.length === 0) return next()
+          // args.length 0 for explicitly calling middleware next()
         })
       }
     } else {
